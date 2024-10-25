9908468103aa89d74ec95bc2ba68b75c41ca5fa0
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-10-25 09:35:37 +0700

    added custom link

---
+++ resources/js/Components/MyLink.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { router } from '@inertiajs/vue3';
+
+const props = defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  method: {
+    type: String,
+    default: 'get',
+  },
+  label: {
+    type: String,
+    default: '',
+  }
+});
+
+</script>
+
+<template>
+  <a @click.prevent="router[props.method](props.href)" :href="props.href">
+    <template v-if="props.label">
+      {{ props.label }}
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
+  </a>
+</template>
+
+<style scoped>
+a {
+  cursor: pointer;
+}
+</style>
