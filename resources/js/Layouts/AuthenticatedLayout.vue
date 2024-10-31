6644da0ca3b6086da4d2509a69161d2df556507a
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-11-01 06:02:24 +0700

    updated side menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="hHh LpR fFf">
+  <q-layout view="lHh LpR lFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
@@ -8,18 +8,68 @@
     </q-header>
 
     <q-drawer :breakpoint="768" v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header>{{ user.email }}</q-item-label>
-        <q-item v-for="item in navItems" :key="item.name" clickable exact
-          @click="item.method ? router[item.method](item.path) : router.get(item.path)">
-          <q-item-section avatar>
-            <q-icon :name="item.icon" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ item.label }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
+      <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+          <div class="text-weight-bold">{{ user.name }}</div>
+          <div>{{ user.email }}</div>
+        </div>
+      </q-img>
+      <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+        <q-list padding dense>
+          <q-item clickable v-ripple :active="true" @click="router.get(route('dashboard'))">
+            <q-item-section avatar>
+              <q-icon name="dashboard" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Dashboard</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple :active="false" @click="router.get('/about')">
+            <q-item-section avatar>
+              <q-icon name="info" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>About</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple @click="router.post(route('logout'))">
+            <q-item-section avatar>
+              <q-icon name="logout" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Logout</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-expansion-item dense expand-separator icon="mail" label="Inbox">
+            <q-expansion-item dense :header-inset-level="0.55" expand-separator icon="receipt" label="Receipts">
+              <q-expansion-item dense switch-toggle-side dense-toggle label="Today" :header-inset-level="0.55"
+                :content-inset-level="1.1">
+                <q-item dense>To</q-item>
+              </q-expansion-item>
+              <q-expansion-item dense switch-toggle-side dense-toggle label="Yesterday" :header-inset-level="0.55"
+                :content-inset-level="1.1">
+                <q-item dense>To</q-item>
+              </q-expansion-item>
+
+            </q-expansion-item>
+
+            <q-expansion-item dense :header-inset-level="0.55" :content-inset-level="1.1" expand-separator icon="schedule"
+              label="Postponed">
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                  commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                  eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+          </q-expansion-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -50,40 +100,13 @@ defineComponent({
 });
 
 const leftDrawerOpen = ref(false);
-const navItems = ref([
-  {
-    name: "dashboard",
-    label: "Dashboard",
-    icon: "dashboard",
-    path: route('dashboard'),
-  },
-  {
-    name: "about",
-    label: "About",
-    icon: "info",
-    path: "/about",
-  },
-  {
-    name: "user-list",
-    label: "Users",
-    icon: "people",
-    path: "/user",
-  },
-  {
-    name: "logout",
-    label: "Logout",
-    icon: "logout",
-    path: route('logout'),
-    method: 'post'
-  },
-]);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
 
-<style>
+<style scoped>
 @media screen and (min-width: 768px) {
   .q-footer {
     display: none;
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return inertia('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -14,11 +13,14 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return inertia('Dashboard');
+    })->name('dashboard');
+    Route::get('/about', function () {
+        return inertia('About');
+    })->name('about');
 
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
