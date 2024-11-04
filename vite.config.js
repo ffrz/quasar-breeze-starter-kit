3d7f796806151f62ad2cdfba5f1b73a4deb4c691
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-11-05 04:48:54 +0700

    fix quasar variables not loaded

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         // @quasar/plugin-vite options list:
         // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
         quasar({
-            sassVariables: ["resources/css/quasar-variables.sass"],
+            sassVariables: "/resources/css/quasar-variables.sass",
         }),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
